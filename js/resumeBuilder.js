9f683e4d36f91d72f20e5b533770d411c53bbe64
rdavis7408 <rdavis7408@gmail.com>
2014-11-13 14:48:37 -0600

    Revert "Initial project commit."
    This reverts commit 0eeb7441e7a9a3ebeeccb20c52fb0c7fc397b1d3.
    
    Conflicts:
    	js/helper.js
    	js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,12 @@
-$("#main").append("Robert Davis");
+
+var name = "Robert L. Davis";	
+var formattedName = HTMLheaderName.replace("%data%", name);
+var role = "Senior Business Analyst";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+
